79f7379ad9935472df84aae089207a79598e6929
Vincent Nwonah <vnwonah@outlook.com>
2019-03-23 00:10:26 +0100

    Added DB Contexts

---
+++ MT-NETCore-Data/CatalogDB/Tenants.cs
@@ -0,0 +1,9 @@
+﻿namespace MTNETCoreData.CatalogDB
+{
+    public partial class Tenants
+    {
+        public byte[] TenantId { get; set; }
+        public string TenantName { get; set; }
+        public string ServicePlan { get; set; }
+    }
+}
