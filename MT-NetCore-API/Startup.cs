dd0374eac5939ad9a2bb2a766e9728b61b1c6fbe
Vincent Nwonah <vnwonah@outlook.com>
2019-03-23 14:56:47 +0100

    added swagger

--- MT-NetCore-API/Startup.cs
+++ MT-NetCore-API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace MT_NetCore_API
 {
@@ -26,6 +27,11 @@ namespace MT_NetCore_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "MobileForms API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +47,14 @@ namespace MT_NetCore_API
                 app.UseHsts();
             }
 
+
             app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MobileForms API");
+                c.RoutePrefix = string.Empty;
+            });
             app.UseMvc();
         }
     }
