203f5c38614d966390b7f2c90016c966eed5948a
Vincent Nwonah <vnwonah@outlook.com>
2019-04-02 12:07:32 +0100

    modified register model

--- MT-NetCore-API/Models/AuthModels/LoginModel.cs
+++ MT-NetCore-API/Models/AuthModels/LoginModel.cs
@@ -12,7 +12,6 @@ namespace MT_NetCore_API.Models.AuthModels
 
         [Required]
         [JsonProperty("password")]
-        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$")]
         public string Password { get; set; }
     }
 }
--- MT-NetCore-API/Models/AuthModels/RegisterModel.cs
+++ MT-NetCore-API/Models/AuthModels/RegisterModel.cs
@@ -7,30 +7,38 @@ namespace MT_NetCore_API.Models.AuthModels
     {
         [Required]
         [JsonProperty("email")]
+        [EmailAddress]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
         [JsonProperty("password")]
         [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$", 
+            ErrorMessage = "Password must be at least 8 characters " +
+        	"and must contain at least 1 uppercase letter, 1 lowercase letter, 1 numerical value and 1 special character")]
         public string Password { get; set; }
 
         [JsonProperty("confirm_password")]
         [DataType(DataType.Password)]
         [Compare(nameof(Password))]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+            ErrorMessage = "Password must be at least 8 characters " +
+            "and must contain at least 1 uppercase letter, 1 lowercase letter, 1 numerical value and 1 special character")]
         public string ConfirmPassword { get; set; }
 
         [Required]
-        [RegularExpression("^[a-z]{3,10}$", ErrorMessage = "Please enter a valid name with 1 - 10 letters")]
+        [RegularExpression("^[\\w'\\-,.][^0-9_!¡?÷?¿/\\\\+=@#$%ˆ&*(){}|~<>;:[\\]]{2,}$", ErrorMessage = "Please enter a valid name with bwtween 3 - 10 letters")]
         [JsonProperty("first_name")]
         public string FirstName { get; set; }
 
         [Required]
-        [RegularExpression("^[a-z]{3,10}$", ErrorMessage = "Please enter a valid name with 1 - 10 letters")]
+        [RegularExpression("^[\\w'\\-,.][^0-9_!¡?÷?¿/\\\\+=@#$%ˆ&*(){}|~<>;:[\\]]{2,}$", ErrorMessage = "Please enter a valid name with bwtween 3 - 10 letters")]
         [JsonProperty("last_name")]
         public string LastName { get; set; }
 
         [Required]
         [JsonProperty("phone_number")]
+        //[RegularExpression("/^\\s*(?:\\+?(\\d{1,3}))?([-. (]*(\\d{3})[-. )]*)?((\\d{3})[-. ]*(\\d{2,4})(?:[-.x ]*(\\d+))?)\\s*$/gm")]
         public string PhoneNumber { get; set; }
     }
 }
