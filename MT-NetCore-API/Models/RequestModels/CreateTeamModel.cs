008b8dcf974f2fbbfa4539bc5bd3090ef6711b3e
Vincent Nwonah <vnwonah@outlook.com>
2019-04-08 17:11:00 +0100

    added auto properties

---
+++ MT-NetCore-API/Models/RequestModels/CreateTeamModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MT_NetCore_API.Models.RequestModels
+{
+    public class CreateTeamModel
+    {
+        public CreateTeamModel()
+        {
+        }
+    }
+}
---
+++ MT-NetCore-API/Swagger/SchemaFilter.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MT_NetCore_API.Swagger
+{
+    public class SchemaFilter
+    {
+        public SchemaFilter()
+        {
+        }
+    }
+}
