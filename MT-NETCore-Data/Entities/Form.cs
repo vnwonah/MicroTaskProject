cf940c8f875f494c24d7b449899512345f2f5bff
Vincent Nwonah <vnwonah@outlook.com>
2019-05-13 14:16:43 +0100

    edited form

--- MT-NETCore-Data/Entities/Form.cs
+++ MT-NETCore-Data/Entities/Form.cs
@@ -1,17 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace MT_NetCore_Data.Entities
 {
     public class Form : BaseEntity
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
 
+        [JsonProperty("title")]
         public string Title { get; set; }
 
+        [JsonProperty("form_json")]
         public string FormJson { get; set; }
 
         public List<Location> CollectionLocations { get; set; }
-        
+
+        [JsonProperty("project_id")]
         public int ProjectId { get; set; }
         
         public List<User> Users { get; set; }
