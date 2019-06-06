3b312698a9fc8a0b1241cfb73b47de631c832097
Vincent Nwonah <vnwonah@outlook.com>
2019-06-06 05:57:23 +0100

    added record's controller

---
+++ MT-NetCore-API/Controllers/RecordController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MT_NetCore_API.Interfaces;
+using Newtonsoft.Json.Linq;
+
+namespace MT_NetCore_API.Controllers
+{
+    [Authorize(Policy = "ApiUser")]
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class RecordController : BaseController
+    {
+        public RecordController(
+            IRequestContext requestContext) 
+            : base(requestContext)
+        {
+                
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]JObject model)
+        {
+           return new OkResult();
+        }
+    }
+}
