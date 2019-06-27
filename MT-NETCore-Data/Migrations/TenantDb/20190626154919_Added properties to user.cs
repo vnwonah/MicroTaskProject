21fc2e119602a1a6adb9975868408ebc3bdf1825
Vincent Nwonah <vnwonah@outlook.com>
2019-06-27 10:24:30 +0100

    User model update

---
+++ MT-NETCore-Data/Migrations/TenantDb/20190626154919_Added properties to user.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MT_NetCore_Data.Migrations.TenantDb
+{
+    public partial class Addedpropertiestouser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccountNumber",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "BankName",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccountNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "BankName",
+                table: "Users");
+        }
+    }
+}
